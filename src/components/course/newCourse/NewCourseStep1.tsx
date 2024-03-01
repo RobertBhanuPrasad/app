eaa8e134dd147eedd1e4e16c041e37eeccfc5943
Ambica Supraja <karumurisupraja@gmail.com>
2024-03-01 14:15:53 +0530

    feat: fix drop downs

--- src/components/course/newCourse/NewCourseStep1.tsx
+++ src/components/course/newCourse/NewCourseStep1.tsx
@@ -1,13 +1,42 @@
 import Coteacher from "@public/assets/Coteacher";
 import Organizer from "@public/assets/Organizer";
 import Teacher from "@public/assets/Teacher";
+import { useSelect } from "@refinedev/core";
 import React, { useState } from "react";
 import { Card } from "src/ui/card";
+import CustomSelect from "src/ui/custom-select";
 import { Label } from "src/ui/label";
+import { MultiSelect } from "src/ui/multi-select";
 import { RadioGroup, RadioGroupItem } from "src/ui/radio-group";
 
 function NewCourseStep1() {
   const [selectedRadioValue, setRadioSelectedValue] = useState("option-one");
+
+  const [organization, setOrganization] = useState();
+
+  const [teachers, setTeachers] = useState();
+
+  const { options, onSearch } = useSelect({
+    resource: "organizations",
+    optionLabel: "name",
+    optionValue: "id",
+    onSearch: (value) => [
+      {
+        field: "name",
+        operator: "contains",
+        value,
+      },
+    ],
+  });
+
+  const data = [
+    { label: "A", value: "a" },
+    { label: "B", value: "b" },
+    { label: "C", value: "c" },
+    { label: "D", value: "d" },
+  ];
+
+  console.log(options, "options");
 
   return (
     <div>
@@ -20,8 +49,10 @@ function NewCourseStep1() {
       >
         <div className="flex items-center flex-row gap-7">
           <Card
-            className={` p-2 w-80 h-[106px] ${
-              selectedRadioValue === "option-one" ? "border-[#7677F4]" : ""
+            className={` p-2 w-80 h-[106px] flex flex-row ${
+              selectedRadioValue === "option-one"
+                ? "border-[#7677F4] shadow-md shadow-[#7677F450]  "
+                : ""
             }`}
           >
             <div>
@@ -35,7 +66,7 @@ function NewCourseStep1() {
                 }
               />
             </div>
-            <div className="flex flex-col items-center gap-[16px]">
+            <div className="flex flex-col items-center gap-[16px]  w-full justify-center">
               <Teacher
                 color={` ${
                   selectedRadioValue === "option-one" ? "#7677F4" : "#999999"
@@ -53,8 +84,10 @@ function NewCourseStep1() {
           </Card>
 
           <Card
-            className={` p-2 gap-2 w-80 h-[106px]  ${
-              selectedRadioValue === "option-two" ? "border-[#7677F4]" : ""
+            className={` p-2 gap-2 w-80 h-[106px] flex flex-row ${
+              selectedRadioValue === "option-two"
+                ? "border-[#7677F4] shadow-md shadow-[#7677F450] "
+                : ""
             }`}
           >
             <RadioGroupItem
@@ -66,24 +99,28 @@ function NewCourseStep1() {
                   : "border !border-[#D6D7D8] border-[1.5px]"
               }
             />
-            <Label
-              htmlFor="option-two"
-              className={`text-[#333333] font-normal ${
-                selectedRadioValue === "option-two" ? "text-[#7677F4]" : ""
-              }`}
-            >
-              I am co-teaching this course
-            </Label>
-            <Coteacher
-              color={` ${
-                selectedRadioValue === "option-two" ? "#7677F4" : "#999999"
-              }`}
-            />
+            <div className="flex flex-col items-center gap-[16px]  w-full justify-center">
+              <Coteacher
+                color={` ${
+                  selectedRadioValue === "option-two" ? "#7677F4" : "#999999"
+                }`}
+              />
+              <Label
+                htmlFor="option-two"
+                className={`text-[#333333] font-normal ${
+                  selectedRadioValue === "option-two" ? "text-[#7677F4]" : ""
+                }`}
+              >
+                I am co-teaching this course
+              </Label>
+            </div>
           </Card>
 
           <Card
-            className={`flex justify-center p-2 gap-2 w-80 h-[106px] ${
-              selectedRadioValue === "option-three" ? "border-[#7677F4]" : ""
+            className={`flex justify-center p-2 gap-2 w-80 h-[106px] flex flex-row ${
+              selectedRadioValue === "option-three"
+                ? "border-[#7677F4] shadow-md shadow-[#7677F450] "
+                : ""
             }`}
           >
             <RadioGroupItem
@@ -95,22 +132,57 @@ function NewCourseStep1() {
                   : "border !border-[#D6D7D8] border-[1.5px]"
               }
             />
-            <Label
-              htmlFor="option-three"
-              className={`text-[#333333] font-normal ${
-                selectedRadioValue === "option-three" ? "text-[#7677F4]" : ""
-              }`}
-            >
-              I am organizing this course for another teacher
-            </Label>
-            <Organizer
-              color={` ${
-                selectedRadioValue === "option-three" ? "#7677F4" : "#999999"
-              }`}
-            />
+            <div className="flex flex-col items-center gap-[16px]  w-full justify-center">
+              <Organizer
+                color={` ${
+                  selectedRadioValue === "option-three" ? "#7677F4" : "#999999"
+                }`}
+              />
+              <Label
+                htmlFor="option-three"
+                className={`text-[#333333] font-normal ${
+                  selectedRadioValue === "option-three" ? "text-[#7677F4]" : ""
+                }`}
+              >
+                I am organizing this course for another teacher
+              </Label>
+            </div>
           </Card>
         </div>
       </RadioGroup>
+      <div className="mt-8 flex flex-row gap-7 ">
+        <div className="flex gap-1 flex-col">
+          <div className="text-xs font-normal text-[#333333]">
+            Organization *
+          </div>
+          <CustomSelect
+            value={organization}
+            placeholder="Select Organization"
+            data={options}
+            onBottomReached={() => {}}
+            onSearch={(val: string) => {
+              onSearch(val);
+            }}
+            onChange={(val) => {
+              console.log(val, "Value is multi select");
+            }}
+          />
+        </div>
+        <div className="flex gap-1 flex-col">
+          <div className="text-xs font-normal text-[#333333]">Program Organizer *</div>
+          <MultiSelect
+            value={teachers}
+            placeholder="Enter Program Name"
+            data={data}
+            onBottomReached={() => {}}
+            onSearch={() => {}}
+            onChange={() => {}}
+          />
+        </div>
+      </div>
+      <div>
+      Registration via 3rd party gateway
+      </div>
     </div>
   );
 }
