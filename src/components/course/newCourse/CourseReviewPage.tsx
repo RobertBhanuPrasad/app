5f9dc1241c325af3445527a36e0c4112c776d545
sunilkalikayi2580@gmail.com <sunilkalikayi2580@gmail.com>
2024-04-02 09:36:36 +0530

    feat: work on the review page add the data rendering of english create new file for the review page

---
+++ src/components/course/newCourse/CourseReviewPage.tsx
@@ -0,0 +1,18 @@
+export default function CourseReviewPage() {
+    return (
+        <main className="w-full max-h-fit">
+            {/* Basic Details */}
+            <section></section>
+            {/* Course Details */}
+            <section></section>
+            {/* Time and Venue */}
+            <section></section>
+            {/* Fees Information */}
+            <section></section>
+            {/* Accommodation Information */}
+            <section></section>
+            {/* Contact Info */}
+            <section></section>
+        </main>
+    );
+}
