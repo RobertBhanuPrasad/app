dd32f09d45cb4b3828da95dd8f6fff623fcbb1c2
rajkiran686 <gutturthirajkiran143@gmail.com>
2024-04-06 00:02:29 +0530

    update the dialog content

--- pages/Courses/FindCourse/Columns.tsx
+++ pages/Courses/FindCourse/Columns.tsx
@@ -1,4 +1,3 @@
-import { CaretSortIcon } from "@radix-ui/react-icons";
 import { ColumnDef } from "@tanstack/react-table";
 import {
   ArrowDownIcon,
@@ -250,6 +249,7 @@ export const columns: ColumnDef<Program>[] = [
         }
       return (
         <div className="flex justify-center text-primary">
+        
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="ghost" className="h-8 w-8 p-0">
@@ -265,37 +265,8 @@ export const columns: ColumnDef<Program>[] = [
           {isDialogOpen && (
               <Dialog onOpenChange={setIsDialogOpen} open={isDialogOpen}>
               <DialogContent className="sm:max-w-[425px]">
-                <DialogHeader>
-                  <DialogTitle>Edit profile</DialogTitle>
-                  <DialogDescription>
-                    Make changes to your profile here. Click save when you're done.
-                  </DialogDescription>
-                </DialogHeader>
-                <div className="grid gap-4 py-4">
-                  <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="name" className="text-right">
-                      Name
-                    </Label>
-                    <Input
-                      id="name"
-                      defaultValue="Pedro Duarte"
-                      className="col-span-3"
-                    />
-                  </div>
-                  <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="username" className="text-right">
-                      Username
-                    </Label>
-                    <Input
-                      id="username"
-                      defaultValue="@peduarte"
-                      className="col-span-3"
-                    />
-                  </div>
-                </div>
-                <DialogFooter>
-                  <Button type="submit">Save changes</Button>
-                </DialogFooter>
+                <Button>Yes</Button>
+                <Button>No</Button>
               </DialogContent>
             </Dialog>
           )}
