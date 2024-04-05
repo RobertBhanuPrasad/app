398d397244c94393389254beba08b881fd8e83e3
NagaSwapnaDasri <dasarinagaswapna30@gmail.com>
2024-04-05 07:09:56 +0530

    fix: add comment for function

--- src/utility/ValidationSteps.ts
+++ src/utility/ValidationSteps.ts
@@ -2,7 +2,13 @@ import { useFormContext } from "react-hook-form";
 export const useValidateCurrentStepFields = () => {
     const { trigger } = useFormContext();
   
-   
+ /**
+  * @description This function is used for the reference, to check all the fields of particular step is passed the validations or not
+  * It is used in the handling of when the user click on the next button
+  * @function ValidateCurrentStepFields
+  * @param currentStepFormNames 
+  * @returns boolean , this function returns weather all the fields of particular validation is passed or not 
+  */  
  const ValidateCurrentStepFields = async (currentStepFormNames: any[]) => {
     const isAllFieldsValidatedPromise = currentStepFormNames?.reduce(
       async (valid, name) => {
