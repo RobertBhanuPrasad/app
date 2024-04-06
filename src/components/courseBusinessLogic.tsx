7120fcb7fd69f1dae9b69fe65537891204c129b7
sunilkalikayi2580@gmail.com <sunilkalikayi2580@gmail.com>
2024-04-06 13:46:13 +0530

    feat: add api call in thank you page

--- src/components/courseBusinessLogic.tsx
+++ src/components/courseBusinessLogic.tsx
@@ -468,3 +468,26 @@ export const handleTabsBasedOnStatus = (courseStatusId: any, tabIndex: any) => {
     else return true;
   }
 };
+
+/**
+ * Determines the status color based on the provided status ID.
+ * @param statusId The status ID to determine the color for.
+ * @returns An object containing the color code and styles for the status.
+ */
+export const getCourseStatusColorBasedOnStatusId = (statusId: number) => {
+    const courseActiveStatusId = getOptionValueObjectByOptionOrder(PROGRAM_STATUS, ACTIVE)?.id;
+    const coursePendingReviewStatusId = getOptionValueObjectByOptionOrder(PROGRAM_STATUS, PENDING_REVIEW)?.id;
+
+    // Check if the status ID matches the active status ID
+    if (statusId === courseActiveStatusId) {
+        // Return color code and styles for active status
+        return { colorCode: "#15AF53", styles: "text-[#15AF53] bg-[#15AF53]/10" };
+    }
+    // Check if the status ID matches the pending review status ID
+    if (statusId === coursePendingReviewStatusId) {
+        // Return color code and styles for pending review status
+        return { colorCode: "#FFB900", styles: "text-[#FFB900] bg-[#FFB900]/10" };
+    }
+    // If status ID does not match any known status, return undefined
+    // This may indicate an invalid or unknown status ID
+};
