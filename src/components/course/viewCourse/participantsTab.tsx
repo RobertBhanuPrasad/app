ebe4c25cd02c7a90d0667589cebb3ed0c4b6f310
Ambica Supraja <karumurisupraja@gmail.com>
2024-04-06 12:13:16 +0530

    fix: add piechart

--- src/components/course/viewCourse/participantsTab.tsx
+++ src/components/course/viewCourse/participantsTab.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "src/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "src/ui/card";
 import { PieChart } from "react-minimal-pie-chart";
@@ -12,6 +12,7 @@ import { getOptionValuesByOptionLabel } from "src/utility/GetOptionValuesByOptio
 import { generateColors } from "src/utility/GenerateColours";
 import { useList, useOne } from "@refinedev/core";
 import _ from "lodash";
+import { supabaseClient } from "src/utility";
 
 function ParticipantsTab() {
   const genderOptions =
@@ -42,12 +43,28 @@ function ParticipantsTab() {
       select:
         "id,contact_id(gender_id(id,value)),participant_attendence_status_id(id,value),price_category_id(fee_level_id(id,value))",
     },
-  }); 
+  });
 
-  const genderObjects = _.countBy(
-    participantData?.data,  
-    (obj) => obj.contact_id.gender_id?.value
-  );
+  const fetchData = async () => {
+    try {
+      const { data, error } = await supabaseClient.functions.invoke(
+        "get_program_participant_summary",
+        {
+          method: "POST",
+          body: {
+            program_id: 2,
+          },
+        }
+      );
+      console.log(data, "data");
+    } catch (error) {
+      console.error("Error fetching fee data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const participantAttendanceObjects = _.countBy(
     participantData?.data,
@@ -71,17 +88,6 @@ function ParticipantsTab() {
       ...option,
       count: participantAttendanceObjects[option.value] || 0, // Add count from genderCounts object or default to 0 if not found
     }));
- 
-  const genderOptionsWithCounts = genderOptions.map((option: any) => ({
-    ...option,
-    count: genderObjects[option.value] || 0, // Add count from genderCounts object or default to 0 if not found
-  }));
-
-  const data = genderOptionsWithCounts.map((item: any, index: number) => ({
-    title: item.value,
-    value: item.count,
-    color: generateColors(baseColor, genderOptions.length)[index], // Use the index to get the color from the generated colors
-  }));
 
   const participantAttendanceData = participantAttendanceOptionsWithCounts.map(
     (item: any, index: number) => ({
@@ -213,54 +219,111 @@ function ParticipantsTab() {
             </div>
           </CardContent>
         </Card>
-        <Card className="w-[303px] rounded-[15px] border border-[#D9D9D9] drop-shadow-[0_0px_10px_rgba(0,0,0,0.1)] mb-6">
-          <CardHeader>
-            <CardTitle>
-              <div className="text-[18px] font-semibold">Gender Breakdown</div>
-            </CardTitle>
-          </CardHeader>
-          <CardContent className="px-6">
-            <div className="relative">
-              <PieChart
-                data={data}
-                rounded
-                style={{ position: "relative" }}
-                lineWidth={20}
-                paddingAngle={20}
-                radius={25}
-                center={[50, 30]}
-                viewBoxSize={[100, 60]}
-                totalValue={participantData?.total}
-              />
-              <div className="chart-inner-text flex flex-col">
-                <p>Total Participants</p>
-                <p className="chart-inner-value">{participantData?.total}</p>
-              </div>
-            </div>
-            <div className="flex flex-col mt-6">
-              <div className="">
-                {genderOptionsWithCounts?.map((item: any, index: number) => {
-                  return (
-                    <div className="flex flex-row items-center justify-center justify-between">
-                      <div className="flex items-center gap-2 py-[2px]">
-                        <Circle
-                          size={6}
-                          fill={generateColors(baseColor, numColors)[index]}
-                          color={generateColors(baseColor, numColors)[index]}
-                        />
-                        <div className="text-[#999999]">{item?.value}</div>
-                      </div>
-                      <div className="font-semibold">{item?.count}</div>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          </CardContent>
-        </Card>
+        <GenderPieChart />
       </div>
     </div>
   );
 }
 
 export default ParticipantsTab;
+
+const GenderPieChart = () => {
+  const genderOptions =
+    getOptionValuesByOptionLabel(GENDER)?.[0]?.option_values;
+
+  const baseColor = "#7677F4";
+  const numColors = genderOptions.length;
+  const Id = 2;
+
+  const [participantData, setParticipantData] = useState<any>();
+
+  const fetchData = async () => {
+    try {
+      const { data, error } = await supabaseClient.functions.invoke(
+        "get_program_participant_summary",
+        {
+          method: "POST",
+          body: {
+            program_id: 2,
+          },
+        }
+      );
+      setParticipantData(data);
+      console.log(data, "data");
+    } catch (error) {
+      console.error("Error fetching fee data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const genderData = [
+    {
+      title: genderOptions[0]?.value,
+      value: participantData?.Gender?.Female
+        ? participantData?.Gender?.Female
+        : 0,
+      color: "red",
+    },
+    {
+      title: genderOptions[1]?.value,
+      value: participantData?.Gender?.Male ? participantData?.Gender?.Male : 0,
+      color: "green",
+    },
+    {
+      title: genderOptions[2]?.value,
+      value: participantData?.Gender?.Other
+        ? participantData?.Gender?.Other
+        : 0,
+      color: "blue",
+    },
+  ];
+
+  return (
+    <Card className="w-[303px] rounded-[15px] border border-[#D9D9D9] drop-shadow-[0_0px_10px_rgba(0,0,0,0.1)] mb-6">
+      <CardHeader>
+        <CardTitle>
+          <div className="text-[18px] font-semibold">Gender Breakdown</div>
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="px-6">
+        <div className="relative">
+          <PieChart
+            data={genderData}
+            rounded
+            style={{ position: "relative" }}
+            lineWidth={20}
+            paddingAngle={20}
+            radius={25}
+            center={[50, 30]}
+            viewBoxSize={[100, 60]}
+            totalValue={participantData?.totalParticipantCount}
+          />
+          <div className="chart-inner-text flex flex-col">
+            <p>Total Participants</p>
+            <p className="chart-inner-value">
+              {participantData?.totalParticipantCount}
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col mt-6">
+          <div className="">
+            {genderData?.map((item: any, index: number) => {
+              return (
+                <div className="flex flex-row items-center justify-center justify-between">
+                  <div className="flex items-center gap-2 py-[2px]">
+                    <Circle size={6} fill={item?.color} color={item?.color} />
+                    <div className="text-[#999999]">{item?.title}</div>
+                  </div>
+                  <div className="font-semibold">{item?.value}</div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
