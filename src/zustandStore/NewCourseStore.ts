40f790945556afc570047b2ef8e3c953757689c5
Ambica Supraja <karumurisupraja@gmail.com>
2024-04-08 17:52:47 +0530

    fix: approval data

--- src/zustandStore/NewCourseStore.ts
+++ src/zustandStore/NewCourseStore.ts
@@ -3,8 +3,12 @@ import { create } from "zustand";
 interface NewCourseStore {
   viewPreviewPage: boolean;
   viewThankyouPage: boolean;
+  viewSuccessModal: boolean;
+  viewRejectedModal: boolean;
+  setViewRejectedModal: (by: boolean) => void;
   setViewPreviewPage: (by: boolean) => void;
   setViewThankyouPage: (by: boolean) => void;
+  setViewSuccessModal: (by: boolean) => void;
   newCourseData: any;
   setNewCourseData: (by: any) => void;
   currentStep: number; // Corrected the type to number
@@ -18,6 +22,18 @@ export const newCourseStore = create<NewCourseStore>((set) => ({
   viewThankyouPage: false,
   newCourseData: null,
   programId: 1,
+  viewSuccessModal: false,
+  viewRejectedModal: false,
+  setViewRejectedModal: (data: boolean) => {
+    set(() => ({
+      viewRejectedModal: data,
+    }));
+  },
+  setViewSuccessModal: (data: boolean) => {
+    set(() => ({
+      viewSuccessModal: data,
+    }));
+  },
   setViewPreviewPage: (data: boolean) => {
     set(() => ({
       viewPreviewPage: data,
