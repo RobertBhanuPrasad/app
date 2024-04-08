46d7a872319e0ce8237c558dfcd9b17dbad7aff9
sunilkalikayi2580@gmail.com <sunilkalikayi2580@gmail.com>
2024-04-08 13:43:59 +0530

    feat: add the scrool for the layout childen

--- pages/Contacts/index.tsx
+++ pages/Contacts/index.tsx
@@ -1,9 +1,85 @@
-import React from 'react'
-
 function index() {
-  return (
-    <div>Contacts tab</div>
-  )
+    return (
+        <div className="h-full overflow-scroll bg-[red] pb-16">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, amet reprehenderit quos nulla vero ipsum
+            laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse commodi
+            recusandae aliquam sequi consectetur ab, deleniti assumenda nemo. Enim qui iure alias natus nisi minima
+            deserunt sapiente quidem quaerat. In aperiam, repellendus dicta perspiciatis similique ducimus fugiat amet
+            eaque. Quae in quaerat natus. Neque eveniet cupiditate incidunt est commodi pariatur molestias corrupti
+            libero illo nostrum minus consequatur adipisci blanditiis laboriosam quidem voluptatum harum, consequuntur
+            saepe! Ad nobis officiis saepe earum nam repellendus, in natus corrupti quia porro a autem aliquam
+            doloribus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, amet reprehenderit quos nulla
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            commodi recusandae aliquam sequi consectetur ab, deleniti assumenda nemo. Enim qui iure alias natus nisi
+            minima deserunt sapiente quidem quaerat. In aperiam, repellendus dicta perspiciatis similique ducimus fugiat
+            amet eaque. Quae in quaerat natus. Neque eveniet cupiditate incidunt est commodi pariatur molestias corrupti
+            libero illo nostrum minus consequatur adipisci blanditiis laboriosam quidem voluptatum harum, consequuntur
+            saepe! Ad nobis officiis saepe earum nam repellendus, in natus corrupti quia porro a autem aliquam
+            doloribus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, amet reprehenderit quos nulla
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            commodi recusandae aliquam sequi consectetur ab, deleniti assumenda nemo. Enim qui iure alias natus nisi
+            minima deserunt sapiente quidem quaerat. In aperiam, repellendus dicta perspiciatis similique ducimus fugiat
+            amet eaque. Quae in quaerat natus. Neque eveniet cupiditate incidunt est commodi pariatur molestias corrupti
+            libero illo nostrum minus consequatur adipisci blanditiis laboriosam quidem voluptatum harum, consequuntur
+            saepe! Ad nobis officiis saepe earum nam repellendus, in natus corrupti quia porro a autem aliquam
+            doloribus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, amet reprehenderit quos nulla
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            commodi recusandae aliquam sequi consectetur ab, deleniti assumenda nemo. Enim qui iure alias natus nisi
+            minima deserunt sapiente quidem quaerat. In aperiam, repellendus dicta perspiciatis similique ducimus fugiat
+            amet eaque. Quae in quaerat natus. Neque eveniet cupiditate incidunt est commodi pariatur molestias corrupti
+            libero illo nostrum minus consequatur adipisci blanditiis laboriosam quidem voluptatum harum, consequuntur
+            saepe! Ad nobis officiis saepe earum nam repellendus, in natus corrupti quia porro a autem aliquam
+            doloribus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, amet reprehenderit quos nulla
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            commodi recusandae aliquam sequi consectetur ab, deleniti assumenda nemo. Enim qui iure alias natus nisi
+            minima deserunt sapiente quidem quaerat. In aperiam, repellendus dicta perspiciatis similique ducimus fugiat
+            amet eaque. Quae in quaerat natus. Neque eveniet cupiditate incidunt est commodi pariatur molestias corrupti
+            libero illo nostrum minus consequatur adipisci blanditiis laboriosam quidem voluptatum harum, consequuntur
+            saepe! Ad nobis officiis saepe earum nam repellendus, in natus corrupti quia porro a autem aliquam
+            doloribus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, amet reprehenderit quos nulla
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            commodi recusandae aliquam sequi consectetur ab, deleniti assumenda nemo. Enim qui iure alias natus nisi
+            minima deserunt sapiente quidem quaerat. In aperiam, repellendus dicta perspiciatis similique ducimus fugiat
+            amet eaque. Quae in quaerat natus. Neque eveniet cupiditate incidunt est commodi pariatur molestias corrupti
+            libero illo nostrum minus consequatur adipisci blanditiis laboriosam quidem voluptatum harum, consequuntur
+            saepe! Ad nobis officiis saepe earum nam repellendus, in natus corrupti quia porro a autem aliquam
+            doloribus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, amet reprehenderit quos nulla
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam esse
+            vero ipsum laudantium dolorem ex culpa harum, veritatis nobis eligendi iure quia obcaecati magnam essesunil
+        </div>
+    );
 }
 
-export default index
+export default index;
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,44 +1,38 @@
 import { PropsWithChildren } from "react";
 
-import { Breadcrumb } from "../breadcrumb";
-import { Menu } from "../menu";
 import Navbar from "@components/navbar";
-import { useRouter } from "next/router";
 import HomeIcon from "@public/assets/HomeIcon";
-import Image from "next/image";
 import background from "@public/images/background.png";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { Breadcrumb } from "../breadcrumb";
 
 export const Layout: React.FC<PropsWithChildren> = ({ children }) => {
-  const { pathname } = useRouter();
-  const addGapsToPathname = (pathname: any) => {
-    // Split the pathname by '/' and insert a hyphen before and after each '/'
-    const newPathname = pathname.split("/").join(" / ");
+    const { pathname } = useRouter();
+    const addGapsToPathname = (pathname: any) => {
+        // Split the pathname by '/' and insert a hyphen before and after each '/'
+        const newPathname = pathname.split("/").join(" / ");
 
-    return newPathname;
-  };
+        return newPathname;
+    };
 
-  const formattedPthName = addGapsToPathname(pathname);
+    const formattedPthName = addGapsToPathname(pathname);
 
-  return (
-    <div className="layout">
-      <Image src={background} alt="bg" className="w-full -mt-1 !h-[227px]" />
-      <div className="absolute top-0 left-0 w-full z-10 inset-0">
-        <Navbar />
-        <div className="h-[32px] bg-[#F9F9F9] drop-shadow-md flex items-center gap-2 shrink-0  font-normal text-[12px] text-[#7677F4] ">
-          <HomeIcon />
-          <div>
-            Home
-            {formattedPthName}
-          </div>
+    return (
+        <div className="layout">
+            <Image src={background} alt="bg" className="w-full -mt-1 !h-[227px]" />
+            <div className=" absolute top-0 left-0 w-full h-[100vh] overflow-hidden pb-16">
+                <Navbar />
+                <div className="h-[32px] bg-[#F9F9F9] drop-shadow-md flex items-center gap-2 shrink-0  font-normal text-[12px] text-[#7677F4] ">
+                    <HomeIcon />
+                    <div>
+                        Home
+                        {formattedPthName}
+                    </div>
+                </div>
+                <Breadcrumb />
+                {children}
+            </div>
         </div>
-        <Breadcrumb />
-        <div className="mt-5">
-          <div className="mx-8">
-            <div className="text-[24px] my-4 font-semibold"></div>
-            {children}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
