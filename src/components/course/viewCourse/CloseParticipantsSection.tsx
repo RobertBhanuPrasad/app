ae0b3832f17ab98619a7a45e7e70e6070860481c
nagaraju <balanagaraju2506@gmail.com>
2024-04-13 09:44:30 +0530

    feat: form intialized

--- src/components/course/viewCourse/CloseParticipantsSection.tsx
+++ src/components/course/viewCourse/CloseParticipantsSection.tsx
@@ -5,12 +5,19 @@ import { Button } from "src/ui/button";
 function CloseParticipantsSection() {
   const searchParams = useSearchParams();
   const pathname = usePathname();
-  const { replace, back } = useRouter();
-  function setParamValue(term: string) {
+
+  const { replace } = useRouter();
+
+  /**
+   * When user click on next button we have to change the close_registration section to revenue section
+   * For that we are updating current_section params
+   */
+  function handleNextClick() {
     const params = new URLSearchParams(searchParams);
-    if (term) {
-      params.set("current_section", term);
-    }
+
+    params.set("current_section", "revenue");
+
+    // we have to use this replace method 
     replace(`${pathname}?${params.toString()}`);
   }
 
@@ -27,9 +34,7 @@ function CloseParticipantsSection() {
       </Button>
       <Button
         className="w-[87px] h-[46px]  bg-[#7677F4] rounded-[12px] text-[white]"
-        onClick={() => {
-          setParamValue("revenue");
-        }}
+        onClick={handleNextClick}
       >
         Next
       </Button>
--- src/components/course/viewCourse/CourseAccountingFormTab.tsx
+++ src/components/course/viewCourse/CourseAccountingFormTab.tsx
@@ -4,18 +4,26 @@ import { Button } from "src/ui/button";
 
 function CourseAccountingFormTab() {
   const router = useRouter();
-  console.log(router.asPath, "router");
+
+  /**
+   * Function to handle click of continue button
+   * We have to navigate to the close participants section out of 3 sections
+   * other sections also we have  1. close_Registration section 2. revenue_section 3. expense_section
+   */
+  const handleClickContinue = () => {
+    router.push(
+      `${router.asPath}/course-accounting-form?current_section=close_participants`
+    );
+  };
 
   return (
     <div>
-      rulesSection
+      {/* //TODO: We have to keep rule sction code here */}
+      <div>rulesSection</div>
+
       <Button
         className="bg-[#7677F4] text-[white]"
-        onClick={() => {
-          router.push(
-            `${router.asPath}/course-accounting-form?current_section=close_participants`
-          );
-        }}
+        onClick={handleClickContinue}
       >
         Continue
       </Button>
