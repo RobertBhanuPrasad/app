a7f2207012e38ae6667433c2823001be00644b9c
ManuGM-dev <63191789+ManuGM-dev@users.noreply.github.com>
2024-04-14 22:08:20 -0400

    svgs moved to assests and text tags used

---
+++ public/assets/RedReverseIcon.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function RedReverseIcon() {
+  return (
+    <div>
+         <svg
+            width="24"
+            height="28"
+            viewBox="0 0 24 28"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect width="24" height="28" fill="white" />
+            <circle cx="12" cy="14" r="12" fill="#EC7357" />
+            <path
+              d="M12 12.0556H9.6006L9.6 19.3889H8.4V12.0556H6L9 9L12 12.0556ZM18 16.9444L15 20L12 16.9444H14.4V9.61111H15.6V16.9444H18Z"
+              fill="white"
+            />
+          </svg>
+    </div>
+  );
+}
+
+export default RedReverseIcon;
--- src/components/participants/BalanceDuePopover.tsx
+++ src/components/participants/BalanceDuePopover.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Popover, PopoverTrigger, PopoverContent } from '../../../src/ui/popover'
 import CrossIcon from "@public/assets/CrossIcon";
+import { Header, Text } from '../../../src/ui/TextTags';
 
 export default function BalanceDue({balanceDue}) {
   const [open, setOpen] = useState(false);
@@ -20,24 +21,24 @@ export default function BalanceDue({balanceDue}) {
             <CrossIcon width={16} height={16} />
         </div> 
         </div>
-          <div className="text-[#333333] text-center">
-            <div className="font-semibold text-lg leading-6">Balance due fee component details</div>
-            <div className="font-normal text-xs italic leading-4">(Additional payment request)</div>
+          <div className="text-center">
+            <Header className="leading-6">Balance due fee component details</Header>
+            <Text className="font-normal text-xs italic leading-4">(Additional payment request)</Text>
           </div>
           
           <div className="">
             <div className="flex flex-col gap-2.5">
             <div className="flex justify-between">
-              <span className="font-semibold text-base leading-5">Organization fee:</span>
-              <span className="font-normal text-base leading-5">EUR 1,000.00</span>
+              <Text className="font-semibold text-base leading-5">Organization fee:</Text>
+              <Text className="font-normal text-base leading-5">EUR 1,000.00</Text>
             </div>
             <div className="flex justify-between">
-              <span className="font-semibold text-base leading-5">Accommodation fee:</span>
-              <span className="font-normal text-base leading-5">EUR 100.00</span>
+              <Text className="font-semibold text-base leading-5">Accommodation fee:</Text>
+              <Text className="font-normal text-base leading-5">EUR 100.00</Text>
             </div>
             <div className="flex justify-between">
-              <span className="font-semibold text-base leading-5">Total:</span>
-              <span className="font-normal text-base leading-5">EUR 1,100.00</span>
+              <Text className="font-semibold text-base leading-5">Total:</Text>
+              <Text className="font-normal text-base leading-5">EUR 1,100.00</Text>
             </div>
           </div>
           </div>
