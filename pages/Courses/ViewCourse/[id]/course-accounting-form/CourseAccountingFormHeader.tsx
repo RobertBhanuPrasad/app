0d4609de7245207c1451336a53eceebb305086c9
V Bulli Narasanna <narasanna11421@gmail.com>
2024-04-15 11:04:52 +0530

    fix(MVP-818): PR comments update

---
+++ pages/Courses/ViewCourse/[id]/course-accounting-form/CourseAccountingFormHeader.tsx
@@ -0,0 +1,146 @@
+import Back from '@public/assets/Back'
+import Important from '@public/assets/Important'
+import ParticipantsIcon from '@public/assets/ParticipantsIcon'
+import Line from '@public/assets/line'
+import { useList, useOne } from '@refinedev/core'
+import { useRouter } from 'next/router'
+import { ActionsDropDown } from 'pages/Courses/ViewCourse/[id]'
+import { useEffect, useState } from 'react'
+import CurrencyIcon from 'src/ui/CurrencyIcon'
+import { HoverCard, HoverCardContent, HoverCardTrigger } from 'src/ui/hover-card'
+import { formatDate } from 'src/utility/DateFunctions'
+import { supabaseClient } from 'src/utility/supabaseClient'
+import { newCourseStore } from 'src/zustandStore/NewCourseStore'
+
+function HeaderSection() {
+  const router = useRouter()
+  const Id: number | undefined = router?.query?.id ? parseInt(router.query.id as string) : undefined
+
+  const { data: courseData } = useOne({
+    resource: 'program',
+    id: Id,
+    meta: {
+      select:
+        '*,created_by_user_id(contact_id(full_name)),program_type_id(name,is_approval_required),approved_by_user_id(contact_id(full_name)),program_alias_name_id(id,alias_name),venue_id(*,center_id(id,name),city_id(id,name),state_id(id,name)),status_id(id,value),program_schedules!inner(*)'
+    }
+  })
+  const [participantData, setParticipantData] = useState<any>()
+
+  const fetchData = async () => {
+    try {
+      const { data, error } = await supabaseClient.functions.invoke('get_program_participant_summary', {
+        method: 'POST',
+        body: {
+          program_id: Id
+        }
+      })
+      setParticipantData(data)
+    } catch (error) {
+      console.error('Error fetching fee data:', error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+  const startDate = formatDate(courseData?.data?.program_schedules[0]?.start_time)
+
+  const endDate = formatDate(
+    courseData?.data?.program_schedules[courseData?.data?.program_schedules?.length - 1]?.end_time
+  )
+  const { data: countryConfigData } = useList({
+    resource: 'country_config'
+  })
+  const totalRevenue = participantData?.income
+  const { replace } = useRouter()
+  const { programId } = newCourseStore()
+
+  return (
+    <div className="flex flex-col mx-8">
+      <div className="flex flex-row items-center ">
+        <div
+          className="  text-[#7677F4] pt-3 cursor-pointer"
+          onClick={() => {
+            replace(`/Courses/ViewCourse/${programId}`)
+          }}
+        >
+          <Back />
+        </div>
+        <div className="text-[33px] font-semibold pl-5 ">
+          {courseData?.data?.program_alias_name_id
+            ? courseData?.data?.program_alias_name_id?.alias_name
+            : courseData?.data?.program_type_id?.name}
+        
+        </div>
+        <div className="pr-2 ml-auto  ">
+          <ActionsDropDown courseData={courseData?.data} />
+        </div>
+      </div>
+      <div className="flex flex-row gap-5 ">
+        <p className=" pl-9  text-[#666666] text-[16px]">Course Accounting Form</p>
+        <div className="  ">
+          <Line />
+        </div>
+        <div className=" text-[#333333]">
+          {startDate} to {endDate}
+        </div>
+        <div className="">
+          <Line />
+        </div>
+
+        <div className="">
+          <ParticipantsIcon />
+        </div>
+        <div
+          onClick={() => {
+            router.push('/')
+          }}
+          className="cursor-pointer text-[#7677F4]"
+        >
+          {participantData?.participantCount}
+        </div>
+        <HoverCard>
+          <HoverCardTrigger>
+            <div className=" pt-1">
+              <Important />
+            </div>
+          </HoverCardTrigger>
+          <HoverCardContent>
+            <div className="w-[231px] text-wrap !rounded-[15px] font-normal">
+              {participantData?.participantCount} Participants with: Transaction status = Confirmed / Pending Attendance
+              status = Confirmed / Pending / Dropout Total participants records:
+              {participantData?.totalParticipantCount}
+            </div>
+          </HoverCardContent>
+        </HoverCard>
+        <div className="pt-1">
+          <CurrencyIcon />
+        </div>
+
+        <div
+          onClick={() => {
+            router.push('/')
+          }}
+          className="cursor-pointer text-[#7677F4] "
+        >
+          {countryConfigData?.data?.[0]?.default_currency_code} {totalRevenue}
+        </div>
+        <HoverCard>
+          <HoverCardTrigger>
+            <div className="pt-1">
+              <Important />
+            </div>
+          </HoverCardTrigger>
+          <HoverCardContent>
+            <div className="w-[231px] text-wrap !rounded-[15px] font-normal">
+              Revenue from confirmed pending transaction participants revenue:
+              {countryConfigData?.data?.[0]?.default_currency_code} {totalRevenue}
+            </div>
+          </HoverCardContent>
+        </HoverCard>
+      </div>
+    </div>
+  )
+}
+
+export default HeaderSection
--- pages/Courses/ViewCourse/[id]/course-accounting-form/index.tsx
+++ pages/Courses/ViewCourse/[id]/course-accounting-form/index.tsx
@@ -2,63 +2,14 @@ import Form from '@components/Formfield'
 import CloseParticipantsSection from '@components/course/viewCourse/CloseParticipantsSection'
 import ExpenseSection from '@components/course/viewCourse/ExpenseSection'
 import RevenueSection from '@components/course/viewCourse/RevenueSection'
-import Important from '@public/assets/Important'
-import ParticipantsIcon from '@public/assets/ParticipantsIcon'
-import { useList, useOne } from '@refinedev/core'
 import { useSearchParams } from 'next/navigation'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import CurrencyIcon from 'src/ui/CurrencyIcon'
-import { HoverCard, HoverCardContent, HoverCardTrigger } from 'src/ui/hover-card'
-import Line from 'src/ui/line'
-import { formatDate } from 'src/utility/DateFunctions'
-import { supabaseClient } from 'src/utility/supabaseClient'
-import { ActionsDropDown } from '..'
-import Back from 'src/ui/Back'
+import HeaderSection from './CourseAccountingFormHeader'
 
 // Define type for sectionComponents
 
 function index() {
   const params = useSearchParams()
   const currentSection = params.get('current_section')
-  const router = useRouter()
-
-  const Id: number | undefined = router?.query?.id ? parseInt(router.query.id as string) : undefined
-
-  const { data: courseData } = useOne({
-    resource: 'program',
-    id: Id,
-    meta: {
-      select:
-        '*,created_by_user_id(contact_id(full_name)),program_type_id(name,is_approval_required),approved_by_user_id(contact_id(full_name)),program_alias_name_id(id,alias_name),venue_id(*,center_id(id,name),city_id(id,name),state_id(id,name)),status_id(id,value),program_schedules!inner(*)'
-    }
-  })
-
-  const [participantData, setParticipantData] = useState<any>()
-
-  const fetchData = async () => {
-    try {
-      const { data, error } = await supabaseClient.functions.invoke('get_program_participant_summary', {
-        method: 'POST',
-        body: {
-          program_id: Id
-        }
-      })
-      setParticipantData(data)
-    } catch (error) {
-      console.error('Error fetching fee data:', error)
-    }
-  }
-
-  useEffect(() => {
-    fetchData()
-  }, [])
-  const startDate = formatDate(courseData?.data?.program_schedules[0]?.start_time)
-
-  const endDate = formatDate(
-    courseData?.data?.program_schedules[courseData?.data?.program_schedules?.length - 1]?.end_time
-  )
-
 
   const sections = [
     {
@@ -90,107 +41,13 @@ function index() {
     console.log('form data', data)
   }
 
-  const { data: countryConfigData } = useList({
-    resource: 'country_config'
-  })
-  const totalRevenue = participantData?.income
-  const { replace } = useRouter()
-
-  const headerSection = () => {
-    return (
-      <div className="flex flex-col  pl-5">
-        <div className="flex flex-row item-center justify-between ">
-          <div
-            className="  text-[#7677F4] pt-3 cursor-pointer"
-            onClick={() => {
-              replace('/Courses/ViewCourse/2')
-            }}
-          >
-            <Back />
-          </div>
-
-          <div className="text-[33px] font-semibold pl-5 ">
-            {courseData?.data?.program_alias_name_id
-              ? courseData?.data?.program_alias_name_id?.alias_name
-              : courseData?.data?.program_type_id?.name}
-          </div>
-          <div>
-            <ActionsDropDown courseData={courseData?.data} />
-          </div>
-        </div>
-        <div className="flex flex-row gap-5 ">
-          <p className=" pl-9  text-[#666666] text-[16px]">Course Accounting Form</p>
-          <div className="  ">
-            <Line />
-          </div>
-          <div className=" text-[#333333]">
-            {startDate} to {endDate}
-          </div>
-          <div className="">
-            <Line />
-          </div>
-
-          <div className="">
-            <ParticipantsIcon />
-          </div>
-          <div
-            onClick={() => {
-              router.push('/')
-            }}
-            className="cursor-pointer "
-          >
-            {participantData?.participantCount}
-          </div>
-          <HoverCard>
-            <HoverCardTrigger>
-              <div className=" pt-1">
-                <Important />
-              </div>
-            </HoverCardTrigger>
-            <HoverCardContent>
-              <div className="w-[231px] text-wrap !rounded-[15px] font-normal">
-                {participantData?.participantCount} Participants with: Transaction status = Confirmed / Pending
-                Attendance status = Confirmed / Pending / Dropout Total participants records:
-                {participantData?.totalParticipantCount}
-              </div>
-            </HoverCardContent>
-          </HoverCard>
-          <div className="pt-1">
-            <CurrencyIcon />
-          </div>
-
-          <div
-            onClick={() => {
-              router.push('/')
-            }}
-            className="cursor-pointer"
-          >
-            {countryConfigData?.data?.[0]?.default_currency_code} {totalRevenue}
-          </div>
-          <HoverCard>
-            <HoverCardTrigger>
-              <div className="pt-1">
-                <Important />
-              </div>
-            </HoverCardTrigger>
-            <HoverCardContent>
-              <div className="w-[231px] text-wrap !rounded-[15px] font-normal">
-                Revenue from confirmed pending transaction participants revenue:
-                {countryConfigData?.data?.[0]?.default_currency_code} {totalRevenue}
-              </div>
-            </HoverCardContent>
-          </HoverCard>
-        </div>
-      </div>
-    )
-  }
-
   return (
     <div>
       <section>
         {/* //TODO: here we have to keep header section code here */}
         {/* //TODO: The people we are doing header section US team is already done please dont do again ask them take code and use all only one componet */}
-        {headerSection()}
+
+        <HeaderSection />
       </section>
 
       <Form defaultValues={{}} onSubmit={onSubmit}>
