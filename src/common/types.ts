99ffa089bc83eb9f58dbddb90fa7f50a7b492876
NagaSwapnaDasri <dasarinagaswapna30@gmail.com>
2024-04-17 18:54:51 +0530

    feat(845): add name of the person to reimburse in expense details table

--- src/common/types.ts
+++ src/common/types.ts
@@ -701,17 +701,18 @@ interface CourseAccountingFormFieldTypes {
   program_expenses?: {
     id?: number;
     expense_category?: number;
-    details?:string;
+    details?: string;
     recipt_id?: number;
     purchase_date?: string;
-    amount?:number;
-    reimbursable?:boolean;
-    name_of_person_to_reimbursable?:number;
+    amount?: number;
+    reimbursable?: boolean;
+    name_of_person_to_reimbursable?: number;
     payment_method?: number;
     vat_condition?: number;
     vat_tax_id?: number;
     vendor_name?: string;
-    vat_rate?: string
+    vat_rate?: string;
+    new_person_to_reimburse?: string;
   }[];
   program_offline_revenue?: ProgramOfflineRevenueDatabaseType[];
   course_accounting_user_consent?: boolean;
