f8df21baa5432b84cfb9a8271b29d8109943a9a2
dpraveen123 <dpraveen453@gmail.com>
2024-04-17 23:37:11 -0400

    refactor:solved scrollable tabs issue

--- src/components/participant/viewParticipant/ScrollableTabs.tsx
+++ src/components/participant/viewParticipant/ScrollableTabs.tsx
@@ -1,51 +1,57 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from 'react'
 
-function ScrollableTabs({ tabs }: any) {
-  const [activeTab, setActiveTab] = useState(0);
-  const tabRefs = useRef([]);
+interface Tab {
+  id: number
+  label: string
+  content: JSX.Element
+}
+
+function ScrollableTabs({ tabs }: { tabs: Tab[] }) {
+  const [activeTab, setActiveTab] = useState<number>(0)
+  const tabRefs = useRef<HTMLDivElement[]>([])
 
   useEffect(() => {
     const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
+      entries => {
+        entries.forEach(entry => {
           if (entry.isIntersecting) {
-            setActiveTab(parseInt(entry.target.dataset.tabIndex));
+            const target = entry.target as HTMLElement
+            const tabIndex = parseInt(target.dataset.tabIndex || '0')
+            setActiveTab(tabIndex)
           }
-        });
+        })
       },
       { threshold: 0.5 }
-    );
+    )
 
-    tabRefs.current.forEach((ref) => {
-      observer.observe(ref);
-    });
+    tabRefs.current.forEach(ref => {
+      observer.observe(ref)
+    })
 
     return () => {
-      observer.disconnect();
-    };
-  }, []);
+      observer.disconnect()
+    }
+  }, [])
 
   const scrollToTab = (tabIndex: number) => {
-    const tabElement = tabRefs.current[tabIndex];
-    tabElement.scrollIntoView({ behavior: "smooth", block: "start" }); // Scroll to the start of the tab
-  };
+    const tabElement = tabRefs.current[tabIndex]
+    tabElement?.scrollIntoView({ behavior: 'smooth', block: 'start' }) // Scroll to the start of the tab
+  }
 
   return (
-    <div className="sticky top-0 z-50 bg-white ">
+    <div className="sticky top-0 z-50 bg-white">
       <div className="h-[800px] overflow-y-auto">
-        <div className="flex sticky z-50 top-0 gap-4 py-2 bg-white overflow-x-auto ">
+        <div className="flex sticky z-50 top-0 gap-4 py-2 bg-white overflow-x-auto">
           {/* Ensure horizontal overflow */}
           {tabs.map((tab, index: number) => (
             <button
               key={tab.id}
               className={`px-4 py-2 border-b-2 text-[16px] font-[600] ${
-                activeTab === index
-                  ? "border-[#7677F4] text-[#7677F4]"
-                  : "border-transparent"
+                activeTab === index ? 'border-[#7677F4] text-[#7677F4]' : 'border-transparent'
               } focus:outline-none`}
               onClick={() => {
-                setActiveTab(index);
-                scrollToTab(index);
+                setActiveTab(index)
+                scrollToTab(index)
               }}
             >
               {tab.label}
@@ -61,7 +67,7 @@ function ScrollableTabs({ tabs }: any) {
               className="mb-[20px]"
               key={tab.id}
               id={`tab-${tab.id}`}
-              ref={(el) => (tabRefs.current[index] = el)}
+              ref={el => (tabRefs.current[index] = el as HTMLDivElement)}
               data-tab-index={index}
             >
               {tab.content}
@@ -70,7 +76,7 @@ function ScrollableTabs({ tabs }: any) {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default ScrollableTabs;
+export default ScrollableTabs
