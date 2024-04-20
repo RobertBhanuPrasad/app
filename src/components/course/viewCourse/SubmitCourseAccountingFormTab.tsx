5ba60fa93f98ba19cbe5552da4d6baa90b48caca
rajkiran686 <gutturthirajkiran143@gmail.com>
2024-04-20 18:54:49 +0530

    modify the URL when clicking on the continue button in course accounting form

--- src/components/course/viewCourse/SubmitCourseAccountingFormTab.tsx
+++ src/components/course/viewCourse/SubmitCourseAccountingFormTab.tsx
@@ -1,34 +1,31 @@
-import { useRouter } from "next/router";
-import React from "react";
-import { Button } from "src/ui/button";
+import { useRouter } from 'next/router'
+import { Button } from 'src/ui/button'
 
 function SubmitCourseAccountingFormTab() {
-  const router = useRouter();
+  const router = useRouter()
 
+  const {
+    query: { id }
+  } = router
   /**
    * Function to handle click of continue button
    * We have to navigate to the close participants section out of 3 sections
    * other sections also we have  1. close_Registration section 2. revenue_section 3. expense_section
    */
   const handleClickContinue = () => {
-    router.push(
-      `${router.asPath}/course-accounting-form?current_section=close_participants`
-    );
-  };
+    router.push(`${id}/course-accounting-form?current_section=close_participants`)
+  }
 
   return (
     <div>
       {/* //TODO: We have to keep rule sction code here */}
       <div>rulesSection</div>
 
-      <Button
-        className="bg-[#7677F4] text-[white]"
-        onClick={handleClickContinue}
-      >
+      <Button className="bg-[#7677F4] text-[white]" onClick={handleClickContinue}>
         Continue
       </Button>
     </div>
-  );
+  )
 }
 
-export default SubmitCourseAccountingFormTab;
+export default SubmitCourseAccountingFormTab
