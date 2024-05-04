40986869e6166f1e2f76f882af999299ed75e890
bhargavi-17 <bhargaviannamaneni@gmail.com>
2024-05-04 18:17:37 +0530

    removed setcurrent

--- pages/courses/list/index.tsx
+++ pages/courses/list/index.tsx
@@ -260,36 +260,32 @@ function index() {
   /**
    *This is the query to get data to show in the table
    */
-  const {
-    tableQueryResult: programData,
-    setCurrent: displayDataSetCurrent,
-    setPageSize: displayDataSetPageSize,
-  } = useTable({
-    resource: "program",
-    meta: {
-      select:
-        "*,program_types(name) , state(name) , city(name) , center(name) ,program_teachers!inner(users(contact_id(full_name))) , program_organizers!inner(users(contact_id(full_name))) , program_type_alias_names(alias_name) , visibility_id(id,value),program_schedules!inner(*), program_fee_level_settings(is_custom_fee) , status_id(id,value) ,program_accounting_status_id(id,value)",
-    },
-    pagination: {
-      pageSize: pageSize,
-      current: current,
-    },
-    filters: {
-      permanent: [
-        {
-          field: "id",
-          operator: "in",
-          value: filteredIds,
-        },
-      ],
-    },
-    sorters: {
-      permanent: [
-        // Sorting the program data based on their created date in descending order so that new created program wil be displayed on top
-        { field: "created_at", order: "desc" },
-      ],
-    },
-  });
+  const { tableQueryResult: programData, setPageSize: displayDataSetPageSize } =
+    useTable({
+      resource: "program",
+      meta: {
+        select:
+          "*,program_types(name) , state(name) , city(name) , center(name) ,program_teachers!inner(users(contact_id(full_name))) , program_organizers!inner(users(contact_id(full_name))) , program_type_alias_names(alias_name) , visibility_id(id,value),program_schedules!inner(*), program_fee_level_settings(is_custom_fee) , status_id(id,value) ,program_accounting_status_id(id,value)",
+      },
+      pagination: {
+        pageSize: pageSize,
+      },
+      filters: {
+        permanent: [
+          {
+            field: "id",
+            operator: "in",
+            value: filteredIds,
+          },
+        ],
+      },
+      sorters: {
+        permanent: [
+          // Sorting the program data based on their created date in descending order so that new created program wil be displayed on top
+          { field: "created_at", order: "desc" },
+        ],
+      },
+    });
 
   /**
    * The variable holds whether all rows are selected or not
@@ -476,11 +472,7 @@ function index() {
             rowSelection={rowSelection}
             setRowSelection={setRowSelection}
             checkboxSelection={true}
-            //Here we have to set the current page of the query we use to display data in table and for query we apply filters
-            setCurrent={(number) => {
-              setCurrent(number);
-              displayDataSetCurrent(number);
-            }}
+            setCurrent={setCurrent}
             pageCount={pageCount}
             total={FilterProgramData?.data?.total || 0}
             pageSize={pageSize}
