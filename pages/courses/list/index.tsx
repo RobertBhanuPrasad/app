e5122327c1c8463babb4e329e13aa62398a49942
Sushma <satyasushma.2610@gmail.com>
2024-05-06 12:46:17 +0530

    fix(1195): find course filters

--- pages/courses/list/index.tsx
+++ pages/courses/list/index.tsx
@@ -654,7 +654,7 @@ export const CourseTypeComponent = ({ name }: any) => {
       value={value}
       onValueChange={(val: any) => {
         onChange(val);
-        // we are making the course name value undefined if the course type is changed
+        // we are making the course name value empty string  if the course type is changed so that it will show the placeholder in course name  if we change the value in course type downdown 
         temporaryOnChange('')
       }}
     >
