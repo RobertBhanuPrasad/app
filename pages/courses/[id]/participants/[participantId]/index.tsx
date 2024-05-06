ed4bf8033e8045378a8d5ace7b938a61c68f9ba8
chethan <010chethan@gmail.com>
2024-05-06 16:45:31 +0530

    fix:participant transaction details

--- pages/courses/[id]/participants/[participantId]/index.tsx
+++ pages/courses/[id]/participants/[participantId]/index.tsx
@@ -116,7 +116,7 @@ export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
 
     const translateProps = await serverSideTranslations(
         context.locale ?? "en",
-        ["common", "course.participants","new_strings"]
+        ["common", "course.participants","new_strings","course.view_course"]
     );
 
     if (!authenticated) {
