dbee5f0726cb7cf45cab48928d670e11f134a770
nagaraju <balanagaraju2506@gmail.com>
2024-05-08 19:54:56 +0530

    feat: change any

--- src/components/participant/viewParticipant/ScrollableTabs.tsx
+++ src/components/participant/viewParticipant/ScrollableTabs.tsx
@@ -69,16 +69,16 @@ function ScrollableTabs({ tabs }: { tabs: Tab[] }) {
               className="mb-[20px]"
               key={tab.id}
               id={`tab-${tab.id}`}
-              ref={(el) => (tabRefs.current[index] = el as HTMLDivElement)}
+              ref={(el) => (tabRefs.current[index] = el as any)}
               data-tab-index={index}
             >
               {tab.content}
             </div>
           ))}
         </div>
-        </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default ScrollableTabs;
