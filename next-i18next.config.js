066daee9437ffb313e1f8cad84f054f0fa7a5b2d
Thiru2807 <selvathiru2802@gmail.com>
2024-05-09 17:48:54 +0530

    pt json translations

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "ca-en", "ee-en", "fi-en", "cz-en", "pt-en", "hr-en", "lt-en", "zz-en"],
+    locales: ["en", "ca-en", "ee-en", "fi-en", "cz-en", "pt-en", "pt-pt", "hr-en", "lt-en", "zz-en"],
   },
   localePath: path.resolve("./public/locales"),
 };
