91b6ac56da203df8105d402d73a75354732f4641
kharsha6 <20b91a5736@srkrec.ac.in>
2024-05-22 12:45:34 +0530

    pop-up appears

--- src/components/course/newCourse/NewCourseStep6.tsx
+++ src/components/course/newCourse/NewCourseStep6.tsx
@@ -1,13 +1,15 @@
 import Add from "@public/assets/Add";
 import Delete from "@public/assets/Delete";
-import React from "react";
+import React, { useState } from "react";
+import { useTranslation } from 'next-i18next';
 import { useEffect } from "react";
 import { useController, useFieldArray, useFormContext } from "react-hook-form";
 import { NewCourseStep6FormNames } from "src/constants/CourseConstants";
+import { Text } from "src/ui/TextTags";
+import { Button } from 'src/ui/button'
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from 'src/ui/dialog'
 import { Input } from "src/ui/input";
 import { Textarea } from "src/ui/textarea";
-import { useTranslation } from 'next-i18next';
-import { Text } from "src/ui/TextTags";
 
 function NewCourseStep6() {
   const {t} = useTranslation(['common', "course.new_course", "new_strings"])
@@ -16,6 +18,7 @@ function NewCourseStep6() {
     name: NewCourseStep6FormNames.contact,
   });
   const { getValues } = useFormContext();
+  const [isDeleteDialogOpen, setDeleteDialogOpen] = useState(false);
 
   const {
     field: { value: courseEmails, onChange: courseEmailOnChange },
@@ -68,7 +71,9 @@ function NewCourseStep6() {
           {/* Input field for Contact Number */}
           <div className="w-58 h-20 flex gap-1 flex-col">
             <div className="flex flex-row text-xs font-normal text-[#333333] gap-1">
-              {index === 0 ? t("course.new_course:contact_info_tab.contact_number") : `${t("course.new_course:contact_info_tab.contact_number")} ${index + 1}`}{" "}
+              {index === 0
+                ? t('course.new_course:contact_info_tab.contact_number')
+                : `${t('course.new_course:contact_info_tab.contact_number')} ${index + 1}`}
             </div>
             <ContactMobile index={index} />
           </div>
@@ -87,14 +92,43 @@ function NewCourseStep6() {
             )}
             {/* Except for first row we need to show delete icon */}
             {index !== 0 && (
-              <div
-                onClick={() => handleDeleteItem(index)}
-                className="text-[#7677F4]  flex flex-row gap-1 justify-center items-center cursor-pointer"
-              >
-                <Delete />
-                {t("delete_button")}
-              </div>
-            )}
+           <Dialog open={isDeleteDialogOpen} onOpenChange={ setDeleteDialogOpen}>
+             <DialogTrigger
+             onClick={() => {
+              setDeleteDialogOpen(true)
+             }}
+             className="text-[#7677F4] font-normal cursor-pointer flex items-center gap-[6px]"
+             >
+             <Delete />
+             {t('delete_button')}
+             </DialogTrigger>
+             <DialogContent className="w-[414px] h-[189px] !py-6 !px-6 !rounded-[24px]">
+             <DialogHeader>
+             <DialogTitle className="flex justify-center">{t('delete_button')}</DialogTitle>
+             <DialogDescription className="flex justify-center !pt-[14px] text-[16px] text-[#333333]">
+               {t('new_strings:are_you_sure_you_want_to_delete_this_contact')}
+             </DialogDescription>
+             </DialogHeader>
+             <DialogFooter className="w-full flex !justify-center gap-6">
+             <DialogClose>
+             <Button className="border border-[#7677F4] bg-[white] w-[71px] h-[46px] text-[#7677F4] font-semibold">
+                {t('no_button')}
+             </Button>
+             </DialogClose>
+             <DialogClose>
+             <Button
+              className="bg-[#7677F4] w-[71px] h-[46px] rounded-[12px] font-semibold"
+              onClick={() => {
+                handleDeleteItem(index)
+              }}
+             >
+             {t('yes')}
+             </Button>
+             </DialogClose>
+             </DialogFooter>
+             </DialogContent>
+           </Dialog>
+          )}
           </div>
         </div>
       ))}
@@ -195,7 +229,7 @@ export const ContactMobile = ({ index }: any) => {
         }}
         error={error ? true : false}
       />
-      <Text>
+           <Text>
         {error && (
           <span className="text-[#FF6D6D] text-[12px]">{error?.message}</span>
         )}
