525ee75ee647bf97d8e2419602b770e93e7e3e12
sandhyakonda26 <sandhya.konda26@gmail.com>
2024-06-13 10:50:58 +0530

    icon and size is changed

--- public/assets/Contactinfo.tsx
+++ public/assets/Contactinfo.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function ContactInfo({ color }: any) {
+function ContactInfo({ color }: {color:string}) {
     return (
         <div>
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
