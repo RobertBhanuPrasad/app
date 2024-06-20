9951f9d98be4527be8df9b24448794fc8f81ee7d
sangisapusaiteja <tejasai38409@gmail.com>
2024-06-20 21:39:08 +0530

    fix(1929):  created a view table for program

--- pages/courses/[id]/participants/list/index.tsx
+++ pages/courses/[id]/participants/list/index.tsx
@@ -1190,7 +1190,7 @@ const handleExportExcel = async (
           Authorization:
             "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0",
           "country-code": countryCode,
-          "languageCode": languageCode,
+          "language-code": languageCode,
         },
       }
     );
