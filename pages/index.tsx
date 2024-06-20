56eb2969b4896d6135ab541c218aebed3ffefbc0
nagaraju <balanagaraju2506@gmail.com>
2024-06-20 14:41:22 +0530

    feat: add

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,9 @@
-import { useState } from "react";
-import TipTapEditor from "src/ui/TiptapEditor";
 import { Button } from "src/ui/button";
 
 export default function Index() {
-  const [content, setContent] = useState<string>("");
-  const handleContentChange = (reason: any) => {
-    setContent(reason);
-  };
-
-  console.log("content is ", content);
-
   return (
     <div className="text-3xl">
       <Button>Hello</Button>
-      <TipTapEditor
-        content={content}
-        onChange={(newContent: string) => handleContentChange(newContent)}
-      />
     </div>
   );
 }
